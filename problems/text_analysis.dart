eb4e6cf350a3ab48a2974c80a22c2b337ddbeb2e
fatratra-png <nyfatratrar@gmail.com>
2026-09-19 18:37:30 +0300

    chore: hardcode the text analyzer

--- problems/text_analysis.dart
+++ problems/text_analysis.dart
@@ -1,26 +1,53 @@
-void main() {
-  String text = 'Lorem ipsum dolor sit amet Lorem 123';
+class TextAnalyzer {
+  String content;
 
-  int words = text.split(' ').length;
+  TextAnalyzer(this.content);
+}
+
+String mostFrequentWord(TextAnalyzer analyzer) {
+  List<String> wordList = analyzer.content.toLowerCase().split(' ');
+  Map<String, int> counts = {};
+
+  for (String word in wordList) {
+    if (counts.containsKey(word)) {
+      counts[word] = counts[word]! + 1;
+    } else {
+      counts[word] = 1;
+    }
+  }
+
+  String topWord = wordList[0];
+  int topCount = 0;
+  for (String word in counts.keys) {
+    if (counts[word]! > topCount) {
+      topCount = counts[word]!;
+      topWord = word;
+    }
+  }
+
+  return topWord;
+}
+
+void main() {
+  TextAnalyzer analyzer = TextAnalyzer('Lorem ipsum dolor sit amet Lorem 123');
+
+  int words = analyzer.content.split(' ').length;
   int spaces = words - 1;
 
   int letters = 0;
   int digits = 0;
-  for (String c in text.split('')) {
+  for (String c in analyzer.content.split('')) {
     if ('abcdefghijklmnopqrstuvwxyzABCDEFGHIJKLMNOPQRSTUVWXYZ'.contains(c)) {
-      letters = letters + 1;
+      letters++;
     }
     if ('0123456789'.contains(c)) {
-      digits = digits + 1;
+      digits++;
     }
   }
 
-  String top = 'lorem';
-
-  print('Text: $text');
   print('Words: $words');
   print('Letters: $letters');
   print('Digits: $digits');
   print('Spaces: $spaces');
-  print('Most frequent word: $top');
+  print('Most frequent word: ${mostFrequentWord(analyzer)}');
 }
