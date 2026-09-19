79f5e48c306557b669a1d896e69ec07b05e3c4ff
fatratra-png <nyfatratrar@gmail.com>
2026-09-19 13:39:58 +0300

    feat: create basic supermarket system with discount and change

--- problems/supermarket.dart
+++ problems/supermarket.dart
@@ -2,10 +2,27 @@ class Product {
   String name;
   double price;
   int quantity;
+  double get subtotal => price * quantity;
 
   Product({required this.name, required this.price, required this.quantity});
-
-  double get subtotal => price * quantity;
 }
 
+double calculateTotal(List<Product> products) {
+  double total = 0;
+  for (final product in products) {
+    total += product.subtotal;
+  }
+  return total;
+}
 
+double applyDiscount(double total, double discountPercent) {
+  return total - (total * discountPercent / 100);
+}
+
+double calculateChange(double paid, double total) {
+  return paid - total;
+}
+
+void main(){
+      
+}
